0abdf5d1b2fcadbdc138662fec661b7682043a84
DaSherman0210 <tgerman181@gmail.com>
2023-08-14 09:17:47 -0500

    feat: :tada: Learning typescript
    Learning TypeScript

---
+++ dist/tipos.js
@@ -0,0 +1,39 @@
+"use strict";
+console.log("artemis");
+let wonderfulGroup = "hola";
+wonderfulGroup = "Artemis";
+//* TIPOS DE DATOS DE JAVASCRIPT
+//? Number
+let artemisQuantity = 40;
+console.log(typeof (artemisQuantity));
+//? String
+let artemisName = "V1";
+console.log(typeof (artemisName));
+//? Boolean
+let artemisHired = true;
+console.log(typeof (artemisHired));
+//? Null
+let artemisNull = null;
+console.log(typeof (artemisNull));
+//? Undefined
+let artemisUndefined = undefined;
+console.log(artemisUndefined);
+//? Object
+let artemisObject = {};
+console.log(typeof (artemisObject));
+//? Functions
+let artemisFuncion = {};
+console.log(artemisFuncion);
+//* TIPOS DE DATOS TYPESCRIPT AÑADIDOS
+//? Any -> Avoid it
+let artemisSigned;
+console.log(artemisSigned);
+let artemisAny = 10;
+artemisAny = "anything";
+console.log(artemisAny);
+//? Unknown
+//? Never
+//? Arrays
+//? Tuplas
+//? Enums
+//* TIPOS DE DATOS INFERIDOS
---
+++ src/tipos.ts
@@ -0,0 +1,81 @@
+console.log("artemis");
+
+let wonderfulGroup:string = "hola";
+
+wonderfulGroup = "Artemis";
+
+//* TIPOS DE DATOS DE JAVASCRIPT
+
+//? Number
+
+let artemisQuantity:number = 40;
+console.log(typeof(artemisQuantity));
+
+//? String
+
+let artemisName:string = "V1";
+console.log(typeof(artemisName));
+
+//? Boolean
+
+let artemisHired:boolean = true;
+console.log(typeof(artemisHired));
+
+//? Null
+
+let artemisNull:null = null;
+console.log(typeof(artemisNull));
+
+//? Undefined
+
+let artemisUndefined:undefined = undefined;
+console.log(artemisUndefined);
+
+//? Object
+
+let artemisObject:object = {};
+console.log(typeof(artemisObject));
+
+
+//? Functions
+
+let artemisFuncion = {
+    
+};
+console.log(artemisFuncion);
+
+
+//* TIPOS DE DATOS TYPESCRIPT AÑADIDOS
+
+
+//? Any -> Avoid it
+
+let artemisSigned;
+console.log(artemisSigned);
+
+let artemisAny:any = 10;
+artemisAny = "anything";
+console.log(artemisAny);
+
+
+//? Unknown
+
+
+
+//? Never
+
+
+
+//? Arrays
+
+
+
+//? Tuplas
+
+
+
+//? Enums
+
+
+
+//* TIPOS DE DATOS INFERIDOS
